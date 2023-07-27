16f52c14ef012eb5a9fdb93c32ef586c61327be4
Sebastian Gębuś <sebastian.gebus@maem.com>
2023-07-27 09:27:07 +0200

    Add project files.

---
+++ Properties/PostgreDataSource.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using WebCon.WorkFlow.SDK.Common.Model;
+using WebCon.WorkFlow.SDK.DataSourcePlugins;
+using WebCon.WorkFlow.SDK.DataSourcePlugins.Model;
+using Npgsql;
+
+namespace PostgreDataSource
+{
+    public class PostgreDataSource : CustomDataSource<PostgreDataSourceConfig>
+    {
+        public override List<DataSourceColumn> GetColumns()
+        {
+            return new List<DataSourceColumn>()
+            {
+                new DataSourceColumn("Id","Id"),
+                new DataSourceColumn("Name","Name")
+            };
+        }
+
+        public override DataTable GetData(SearchConditions searchConditions)
+        {
+            var result = GenerateDataTable();
+            var ConnectionString = string.Format("Host={0};Username={1};Password={2};Database={3}", Configuration.DbHost, Configuration.DbUser, Configuration.DbPassword, Configuration.DbName);
+
+            var Connection = new NpgsqlConnection(ConnectionString);
+            Connection.Open();
+
+            string SqlQuery = @"
+                        SELECT
+                        '' as Id,
+                        '' as Name
+                        ";
+
+            using (NpgsqlCommand SqlCommand = new NpgsqlCommand(SqlQuery, Connection))
+            {
+                SqlCommand.Prepare();
+
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(SqlCommand);
+
+                DataSet _ds = new DataSet();
+                DataTable _dt = new DataTable();
+
+                da.Fill(_ds);
+                _dt = _ds.Tables[0];
+                result = _dt;
+
+                Connection.Close();
+            }
+
+            return result;
+        }
+        private DataTable GenerateDataTable()
+        {
+            var dt = new DataTable("data");
+            foreach (var column in GetColumns())
+            {
+                dt.Columns.Add(new DataColumn(column.Name));
+            }
+
+            return dt;
+        }
+    }
+}
---
+++ Properties/PostgreDataSourceConfig.cs
@@ -0,0 +1,20 @@
+﻿using WebCon.WorkFlow.SDK.Common;
+using WebCon.WorkFlow.SDK.ConfigAttributes;
+
+namespace PostgreDataSource
+{
+    public class PostgreDataSourceConfig : PluginConfiguration
+    {
+        [ConfigEditableText(DisplayName = "PostgreSQL DB Host", DefaultText = "host", IsRequired = true)]
+        public string DbHost { get; set; }
+
+        [ConfigEditableText(DisplayName = "PostgreSQL DB Name", DefaultText = "db", IsRequired = true)]
+        public string DbName { get; set; }
+
+        [ConfigEditableText(DisplayName = "PostgreSQL DB User", DefaultText = "user", IsRequired = true)]
+        public string DbUser { get; set; }
+
+        [ConfigEditableText(DisplayName = "PostgreSQL DB Password", IsPasswordField = true, IsRequired = true)]
+        public string DbPassword { get; set; }
+    }
+}
